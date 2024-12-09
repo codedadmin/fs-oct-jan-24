e0d10eea71672eefd2a7ab7580ed90f773d41022
Othmaan Al-cheese <saraalenzii52@gmail.com>
2024-12-09 19:08:20 +0300

    don

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ app.get("/", (req, res) => {
   names.push("Moudhi");
   names.push("Omar");
   names.push("Dawood");
+  names.push("SarahM");
+  names.push("Shouq");
   names.push("Sarah");
   names.push("Sarah Alenezi");
+  names.push("Sarah AlRashidi");
+  names.push("Hello...");
   names.push("Hey there! This is Sultan!");
+  names.push("Dingus");
+  names.push("Sarah");
+  names.push("Abdullah Al Abbas");
   names.push("Abdullah Mashaan");
+  names.push("Yousef");
+  names.push("hellooo its jenen!");
+  names.push("Hamad AlKhalaf");
+
   res.send(names);
 });
 
